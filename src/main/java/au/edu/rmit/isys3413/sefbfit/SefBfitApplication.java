14d56c7ca9095c8fdb15a10e27e55582203fabcd
Yu Gan <yugan2005@gmail.com>
2020-09-10 21:54:34 -0700

    initial commit

---
+++ src/main/java/au/edu/rmit/isys3413/sefbfit/SefBfitApplication.java
@@ -0,0 +1,13 @@
+package au.edu.rmit.isys3413.sefbfit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+public class SefBfitApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(SefBfitApplication.class, args);
+  }
+}
---
+++ src/test/java/au/edu/rmit/isys3413/sefbfit/SefBfitApplicationTests.java
@@ -0,0 +1,13 @@
+package au.edu.rmit.isys3413.sefbfit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+
+@SpringBootTest
+class SefBfitApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+}
